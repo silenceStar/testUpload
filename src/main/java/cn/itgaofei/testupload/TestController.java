5c7e231cedee4dc1a5571a81e1eef8f5ba6a77d5
gaofei <781010468@qq.com>
2018-12-08 11:11:50 +0800

    第二次提交

---
+++ src/main/java/cn/itgaofei/testupload/TestController.java
@@ -0,0 +1,14 @@
+package cn.itgaofei.testupload;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
