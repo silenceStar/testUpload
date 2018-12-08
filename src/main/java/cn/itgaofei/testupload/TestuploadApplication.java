c9e9b7c06edaf2a068dfbad888726de129c760f5
gaofei <781010468@qq.com>
2018-12-08 10:44:29 +0800

    初次提交

---
+++ src/main/java/cn/itgaofei/testupload/TestuploadApplication.java
@@ -0,0 +1,12 @@
+package cn.itgaofei.testupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestuploadApplication.class, args);
+    }
+}
